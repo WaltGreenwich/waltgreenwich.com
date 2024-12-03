731ff6d7202c1203c167f7354819405bdc4b676f
WGreenwichChaca <walter.greenwich@gmail.com>
2024-12-02 23:51:48 -0500

    ending hire me button

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import profilePic1 from "../../public/images/profile/developer-profile-1.png";
 import AnimatedText from "@/components/AnimatedText";
 import Link from "next/link";
 import { LinkArrow } from "@/components/Icons";
+import HireMe from "@/components/HireMe";
 
 export default function Home() {
   return (
@@ -54,6 +55,7 @@ export default function Home() {
             </div>
           </div>
         </Layout>
+        <HireMe />
       </main>
     </>
   );

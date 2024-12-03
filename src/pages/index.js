734cb6c26468cc522798c1339bbfea71f890556e
WGreenwichChaca <walter.greenwich@gmail.com>
2024-12-03 00:56:34 -0500

    Add About page

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ import AnimatedText from "@/components/AnimatedText";
 import Link from "next/link";
 import { LinkArrow } from "@/components/Icons";
 import HireMe from "@/components/HireMe";
+import lightBulb from "../../public/images/svgs/miscellaneous_icons_1.svg";
 
 export default function Home() {
   return (
@@ -16,12 +17,12 @@ export default function Home() {
       </Head>
       <main className="flex items-center text-dark w-full min-h-screen">
         <Layout className="pt-0">
-          <div className="flex items-center justify-between w-full">
+          <div className="flex items-center justify-between w-full space-x-16">
             <div className="w-1/2">
               <Image
                 src={profilePic1}
                 alt="WaltGreen"
-                className="w-full h-auto"
+                className="w-full h-auto rounded-2xl"
               />
             </div>
 
@@ -56,6 +57,13 @@ export default function Home() {
           </div>
         </Layout>
         <HireMe />
+        <div className="absolute right-8 bottom-8 inline-block w-24">
+          <Image
+            src={lightBulb}
+            alt="WaltGreenwich"
+            className="w-full h-auto"
+          />
+        </div>
       </main>
     </>
   );

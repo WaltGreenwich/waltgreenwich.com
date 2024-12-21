70a677a758f71f65bf462bdb6b4610a67168992c
WGreenwichChaca <walter.greenwich@gmail.com>
2024-12-20 22:02:55 -0500

    moving forward to the projects section

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,6 +1,34 @@
 import AnimatedText from "@/components/AnimatedText";
 import Head from "next/head";
 import Layout from "@/components/Layout";
+import Link from "next/link";
+import Image from "next/image";
+import { GithubIcon } from "@/components/Icons";
+
+const FeaturedProject = ({ type, title, summary, link, img, github }) => {
+  return (
+    <article>
+      <Link href={link} target="_blank">
+        <Image src={img} alt={title} className="w-full h-auto" />
+      </Link>
+      <div>
+        <span>{type}</span>
+        <Link href={link} target="_blank">
+          <h2>{title}</h2>
+        </Link>
+        <p>{summary}</p>
+        <div>
+          <Link href={github} target="_blank">
+            <GithubIcon />
+          </Link>
+          <Link href={link} target="_blank">
+            Visit Project
+          </Link>
+        </div>
+      </div>
+    </article>
+  );
+};
 
 const projects = () => {
   return (
@@ -15,7 +43,9 @@ const projects = () => {
           <AnimatedText text="Imagination Trumps Knowledge" />
 
           <div className="grid grid-cols-12 gap-24">
-            <div className="col-span-12">Featured Project</div>
+            <div className="col-span-12">
+              <FeaturedProject title="Gericht Restaurant" />
+            </div>
             <div className="col-span-6"> Project-1</div>
             <div className="col-span-6"> Project-2</div>
 

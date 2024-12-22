c49017b5f45908251a082bc17d286fae9a59f77e
WGreenwichChaca <walter.greenwich@gmail.com>
2024-12-21 23:55:12 -0500

    Add Featured project

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -4,24 +4,37 @@ import Layout from "@/components/Layout";
 import Link from "next/link";
 import Image from "next/image";
 import { GithubIcon } from "@/components/Icons";
+import project1 from "../../public/images/projects/gericht-restaurant.png";
 
 const FeaturedProject = ({ type, title, summary, link, img, github }) => {
   return (
-    <article>
-      <Link href={link} target="_blank">
+    <article className="w-full flex items-center justify-between rounded-3xl border border-solid border-dark bg-light shadow-2xl p-12">
+      <Link
+        href={link}
+        target="_blank"
+        className="w-1/2 cursor-pointer overflow-hidden rounded-lg"
+      >
         <Image src={img} alt={title} className="w-full h-auto" />
       </Link>
-      <div>
-        <span>{type}</span>
-        <Link href={link} target="_blank">
-          <h2>{title}</h2>
+      <div className="w-1/2 flex flex-col items-start justify-between pl-6">
+        <span className="text-primary font-medium text-xl">{type}</span>
+        <Link
+          href={link}
+          target="_blank"
+          className="hover:underline underline-offset-2"
+        >
+          <h2 className="my-2 w-full text-left text-4xl font-bold">{title}</h2>
         </Link>
-        <p>{summary}</p>
-        <div>
-          <Link href={github} target="_blank">
+        <p className="my-2 font-medium text-dark">{summary}</p>
+        <div className="mt-2 flex items-center">
+          <Link href={github} target="_blank" className="w-10">
             <GithubIcon />
           </Link>
-          <Link href={link} target="_blank">
+          <Link
+            href={link}
+            target="_blank"
+            className="ml-4 rounded-lg bg-dark text-light p-2 px-6 text-lg font-semibold"
+          >
             Visit Project
           </Link>
         </div>
@@ -40,11 +53,18 @@ const projects = () => {
 
       <main className="w-full mb-16 flex flex-col items-center justify-center">
         <Layout className="pt-16">
-          <AnimatedText text="Imagination Trumps Knowledge" />
+          <AnimatedText text="Imagination Trumps Knowledge" className="mb-16" />
 
           <div className="grid grid-cols-12 gap-24">
             <div className="col-span-12">
-              <FeaturedProject title="Gericht Restaurant" />
+              <FeaturedProject
+                title="Gericht Restaurant"
+                img={project1}
+                summary="A visually appealing and easy-to-use restaurant website built with React.js and Tailwind CSS. It displays the restaurant's menu, photo gallery, and contact information in a clear and accessible way."
+                link="https://restaurant-gericht-waltgreenwich.netlify.app/"
+                github="https://github.com/WaltGreenwich/modern_ui_ux_restaurant"
+                type="Featured Project"
+              />
             </div>
             <div className="col-span-6"> Project-1</div>
             <div className="col-span-6"> Project-2</div>

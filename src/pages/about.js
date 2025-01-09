dfc5ef102447a98b48872876951111d7ea7af642
WGreenwichChaca <walter.greenwich@gmail.com>
2025-01-09 04:07:33 -0500

    adding responsive design

--- src/pages/about.js
+++ src/pages/about.js
@@ -41,7 +41,10 @@ const about = () => {
       </Head>
       <main className="flex w-full flex-col items-center justify-center dark:text-light">
         <Layout className="pt-16">
-          <AnimatedText text="Shaping Digital Futures!" className="mb-16" />
+          <AnimatedText
+            text="Shaping Digital Futures!"
+            className="mb-16 lg:!text-7xl sm:!text-6xl xs:!text-4xl"
+          />
           <div className="grid w-full grid-cols-8 gap-16">
             <div className="col-span-3 flex flex-col items-center justify-start">
               <h2 className="mb-4 text-lg font-bold uppercase text-dark/75 dark:text-light/75">

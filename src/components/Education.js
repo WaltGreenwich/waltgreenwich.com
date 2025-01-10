f700c89dfbe477b5dc45049099de3345da77371b
WGreenwichChaca <walter.greenwich@gmail.com>
2025-01-10 10:53:58 -0500

    add responsive layout for Project section

--- src/components/Education.js
+++ src/components/Education.js
@@ -11,7 +11,7 @@ const Details = ({ school, year, program, certificateLink, logo }) => {
   return (
     <li
       ref={ref}
-      className="my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-center justify-between"
+      className="my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-center justify-between md:w-[80%]"
     >
       <LiIcon reference={ref} />
       <motion.div
@@ -21,14 +21,16 @@ const Details = ({ school, year, program, certificateLink, logo }) => {
       >
         <div className="flex items-center mb-2">
           <Image src={logo} alt="logo" className="w-16 h-16 mr-2 rounded-lg" />
-          <span className="text-gray-600 dark:text-light/75">
+          <span className="text-gray-600 dark:text-light/75 xs:text-sm">
             {year} <br />{" "}
-            <p className="text-xl font-semibold dark:text-primaryDark">
+            <p className="text-xl font-semibold dark:text-primaryDark md:text-sm">
               {school}
             </p>
           </span>
         </div>
-        <h3 className="text-2xl font-bold mb-2">{program}</h3>
+        <h3 className="text-2xl font-bold mb-2 sm:text-xl xs:text-lg">
+          {program}
+        </h3>
 
         <p>
           {certificateLink && (
@@ -57,14 +59,17 @@ const Education = () => {
 
   return (
     <div className="my-64">
-      <h2 className="font-bold text-8xl mb-32 w-full text-center">Education</h2>
-      <div ref={ref} className="w-[75%] mx-auto relative">
+      <h2 className="font-bold text-8xl mb-32 w-full text-center md:text-6xl xs:text-4xl md:mb-16">
+        Education
+      </h2>
+      <div ref={ref} className="w-[75%] mx-auto relative lg:w-[90%] md:w-full">
         <motion.div
           style={{ scaleY: scrollYProgress }}
-          className="absolute left-9 top-0 w-[4px] h-full bg-dark origin-top dark:bg-light"
+          className="absolute left-9 top-0 w-[4px] h-full bg-dark origin-top dark:bg-light
+          md:w-[2px] md:left-[30px] xs:left-[20px]"
         />
 
-        <ul className="w-full flex flex-col items-start justify-between ml-4">
+        <ul className="w-full flex flex-col items-start justify-between ml-4 xs:ml-2">
           <Details
             school="SoyHenry"
             logo={henryLogo}
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -10,12 +10,19 @@ import project3 from "../../public/images/projects/brainwave.png";
 
 const FeaturedProject = ({ type, title, summary, link, img, github }) => {
   return (
-    <article className="w-full flex items-center justify-between rounded-3xl border border-solid border-dark bg-light shadow-2xl p-12 relative rounded-br-2xl dark:bg-dark dark:border-light">
-      <div className="absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2.5rem] bg-dark dark:bg-light rounded-br-3xl" />
+    <article
+      className="w-full flex items-center justify-between rounded-3xl border border-solid border-dark bg-light shadow-2xl p-12 relative rounded-br-2xl dark:bg-dark dark:border-light
+    lg:flex-col lg:p-8 xs:rounded-2xl xs:rounded-br-3xl xs:p-4
+    "
+    >
+      <div
+        className="absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2.5rem] bg-dark dark:bg-light rounded-br-3xl xs:-right-2 xs:h-[102%] xs:w-[100%]
+      "
+      />
       <Link
         href={link}
         target="_blank"
-        className="w-1/2 cursor-pointer overflow-hidden rounded-lg"
+        className="w-1/2 cursor-pointer overflow-hidden rounded-lg lg:w-full"
       >
         <Image
           src={img}
@@ -25,8 +32,8 @@ const FeaturedProject = ({ type, title, summary, link, img, github }) => {
           sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
         />
       </Link>
-      <div className="w-1/2 flex flex-col items-start justify-between pl-6">
-        <span className="text-primary dark:text-primaryDark font-medium text-xl">
+      <div className="w-1/2 flex flex-col items-start justify-between pl-6 lg:w-full lg:pl-0 lg:pt-6">
+        <span className="text-primary dark:text-primaryDark font-medium text-xl xs:text-base">
           {type}
         </span>
         <Link
@@ -34,7 +41,7 @@ const FeaturedProject = ({ type, title, summary, link, img, github }) => {
           target="_blank"
           className="hover:underline underline-offset-2"
         >
-          <h2 className="my-2 w-full text-left text-4xl font-bold dark:text-light">
+          <h2 className="my-2 w-full text-left text-4xl font-bold dark:text-light sm:text-sm">
             {title}
           </h2>
         </Link>
@@ -46,7 +53,8 @@ const FeaturedProject = ({ type, title, summary, link, img, github }) => {
           <Link
             href={link}
             target="_blank"
-            className="ml-4 rounded-lg bg-dark text-light p-2 px-6 text-lg font-semibold dark:bg-light dark:text-dark"
+            className="ml-4 rounded-lg bg-dark text-light p-2 px-6 text-lg font-semibold dark:bg-light dark:text-dark
+            sm:px-4 sm:text-base"
           >
             Visit Project
           </Link>
@@ -106,9 +114,12 @@ const projects = () => {
 
       <main className="w-full mb-16 flex flex-col items-center justify-center dark:text-light">
         <Layout className="pt-16">
-          <AnimatedText text="Imagination Trumps Knowledge" className="mb-16" />
+          <AnimatedText
+            text="Dream it. Build it. Live it"
+            className="mb-16 lg:!text-7xl sm:mb-8 sm:!text-6xl xs:!text-4xl"
+          />
 
-          <div className="grid grid-cols-12 gap-24 gap-y-32">
+          <div className="grid grid-cols-12 gap-24 gap-y-32 xl:gap-x-16 lg:gap-x-8 md:gap-y-24 sm:gap-x-0">
             <div className="col-span-12">
               <FeaturedProject
                 title="Gericht Restaurant"

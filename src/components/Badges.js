4f2d8902c0f1a0034af1054b303db65f750bb409
WGreenwichChaca <walter.greenwich@gmail.com>
2025-12-24 20:18:18 -0500

    Añadiendo proyecto AI email intelligence

--- src/components/Badges.js
+++ src/components/Badges.js
@@ -9,6 +9,15 @@ const techBadges = {
     "https://img.shields.io/badge/Figma-F24E1E?style=flat-square&logo=figma&logoColor=white",
   Appwrite:
     "https://img.shields.io/badge/Appwrite-F02E65?style=flat-square&logo=Appwrite&logoColor=black",
+  Python:
+    "https://img.shields.io/badge/Python-3776AB?style=flat-square&logo=python&logoColor=white",
+  Gemini:
+    "https://img.shields.io/badge/Google_Gemini-8E75B2?style=flat-square&logo=googlegemini&logoColor=white",
+  Streamlit:
+    "https://img.shields.io/badge/Streamlit-FF4B4B?style=flat-square&logo=streamlit&logoColor=white",
+  Pandas:
+    "https://img.shields.io/badge/Pandas-150458?style=flat-square&logo=pandas&logoColor=white",
+  LLM: "https://img.shields.io/badge/Applied_AI-05122A?style=flat-square&logo=brain&logoColor=white",
 };
 
 export const Badge = ({ name }) => {
